ca46b229fa65c17dc9c86074945843518f10913f
Đặng Hoàng Minh <107298518+minhhoccode111@users.noreply.github.com>
2023-01-13 04:52:22 +0700

    working with header

--- assest/actions.js
+++ assest/actions.js
@@ -5,6 +5,12 @@
 //   e.target.previousElementSibling.value = "";
 //   e.target.nextElementSibling.innerHTML = "";
 // }
-const warning = document.querySelectorAll('[class*="warning"]');
+const textWarning = document.querySelectorAll(".text-warning");
+const borderWarning = document.querySelectorAll(".border-warning");
+const btnOutlineWarning = document.querySelectorAll(".btn-outline-warning");
+const warning = [...textWarning, ...borderWarning, ...btnOutlineWarning];
 
+console.log(textWarning);
+console.log(borderWarning);
+console.log(btnOutlineWarning);
 console.log(warning);
