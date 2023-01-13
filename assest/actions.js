13d371837c58980eb83f9099324f679bdd395164
Đặng Hoàng Minh <107298518+minhhoccode111@users.noreply.github.com>
2023-01-14 03:46:30 +0700

    re-do javascript files

--- assest/actions.js
+++ assest/actions.js
@@ -1,10 +1,3 @@
-// const x = document.querySelector("span.top-right");
-// x.addEventListener("click", clearInput);
-
-// function clearInput(e) {
-//   e.target.previousElementSibling.value = "";
-//   e.target.nextElementSibling.innerHTML = "";
-// }
 const textWarning = document.querySelectorAll(".text-warning");
 const borderWarning = document.querySelectorAll(".border-warning");
 const btnOutlineWarning = document.querySelectorAll(".btn-outline-warning");
