42963442cf37a43eb768a682045ce05fa70e5626
Đặng Hoàng Minh <107298518+minhhoccode111@users.noreply.github.com>
2023-01-14 05:14:36 +0700

    working with sidebar

--- assest/actions.js
+++ assest/actions.js
@@ -7,3 +7,14 @@ console.log(textWarning);
 console.log(borderWarning);
 console.log(btnOutlineWarning);
 console.log(warning);
+
+const xButton = document.querySelector(".close-sidebar");
+const hamburgerMenu = document.querySelector('i[title="Menu"]');
+const sidebar = document.querySelector(".aside");
+hamburgerMenu.onclick = () => {
+  sidebar.style.width = "100%";
+  sidebar.style.maxWidth = "300px";
+};
+xButton.onclick = () => {
+  sidebar.style.width = "0";
+};
