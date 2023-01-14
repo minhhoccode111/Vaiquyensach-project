48686d4cd5afff80d15905daca50d6e30a8ef0e6
Đặng Hoàng Minh <107298518+minhhoccode111@users.noreply.github.com>
2023-01-14 20:07:22 +0700

    buttons sidebar

--- assest/actions.js
+++ assest/actions.js
@@ -1,11 +1,15 @@
 const xButton = document.querySelector(".close-sidebar");
 const hamburgerMenu = document.querySelector('i[title="Menu"]');
 const sidebar = document.querySelector(".aside");
+const homeButton = document.querySelector(".btn-home");
 hamburgerMenu.onclick = () => {
   sidebar.style.width = "100%";
   sidebar.style.maxWidth = "300px";
 };
 xButton.onclick = () => {
+  sidebar.style.width = "0";
+};
+homeButton.onclick = () => {
   sidebar.style.width = "0";
 };
 //section to expand hamburger menu when click
@@ -14,3 +18,22 @@ const borderWarning = document.querySelectorAll(".border-warning");
 const btnOutlineWarning = document.querySelectorAll(".btn-outline-warning");
 const warning = [...textWarning, ...borderWarning, ...btnOutlineWarning];
 //section to change website theme when button theme is clicked
+let coll = document.querySelectorAll(".collapsible");
+console.log(coll);
+coll.forEach((button) => {
+  button.addEventListener("click", function () {
+    this.classList.toggle("active");
+    let content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+});
+//section to expand when buttons in sidebar is clicked
