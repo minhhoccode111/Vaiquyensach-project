fd2a0b16c0fec039c42863a2f5d96467d7843cf3
Đặng Hoàng Minh <minhhoccode111@gmail.com>
2024-01-26 20:28:25 +0700

    deployment best practice: rate limit and helmet scripts allowed

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const RateLimit = require('express-rate-limit');
 const debug = require('debug')('mongo-connect');
 const cookieParser = require('cookie-parser');
 const createError = require('http-errors');
@@ -10,15 +11,18 @@ const helmet = require('helmet');
 const path = require('path');
 require('dotenv').config();
 
+// connect database
 const dev_db_url = 'mongodb+srv://minhhoccode111:VHHWA2Um99hLEiA9@cluster0.4wfrd1t.mongodb.net/local_library?retryWrites=true&w=majority';
 
 const mongoDB = process.env.MONGODB_URI || dev_db_url;
 
 main().catch(debug);
+
 async function main() {
   await mongoose.connect(mongoDB);
 }
 
+// route handling
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const catalogRouter = require('./routes/catalog'); // Import routes for "catalog" area of site
@@ -29,22 +33,33 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.disable('x-powered-by'); // reduce fingerprinting
+// reduce fingerprinting
+app.disable('x-powered-by');
 
+// rate limit
+const limiter = RateLimit({ windowMs: 1 * 60 * 1000, max: 20 }); // max 20 per 1 minute
+app.use(limiter);
 // compress responses for performance
 app.use(compression());
-
 // security HTTP header
-app.use(helmet());
-app.use(logger('dev')); // use with 'dev' will log the detail of every request to the console
-app.use(express.json()); // when 'Content-Type' : 'application/json' this middleware will parse the json and put result in the req.body for us to use
-app.use(express.urlencoded({ extended: false })); // when 'Content-Type' : 'application/x-www-form-urlencoded' parse data in body of http request
-app.use(cookieParser()); // this parse cookie from http request and put the result in req.cookie for us to use
-app.use(express.static(path.join(__dirname, 'public'))); //  server every files in 'public' folder, which will help access css files after html files response to the client
+app.use(helmet.contentSecurityPolicy({ directives: { 'script-src': ["'self'", 'code.jquery.com', 'cdn.jsdelivr.net'] } })); // allow jquery & bootstrap scripts
+// use with 'dev' will log the detail of every request to the console
+app.use(logger('dev'));
+// when 'Content-Type' : 'application/json' this middleware will parse the json and put result in the req.body for us to use
+app.use(express.json());
+// when 'Content-Type' : 'application/x-www-form-urlencoded' parse data in body of http request
+app.use(express.urlencoded({ extended: false }));
+// this parse cookie from http request and put the result in req.cookie for us to use
+app.use(cookieParser());
+// server every files in 'public' folder, which will help access css files after html files response to the client
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter); // in this project, route '/' will redirect to '/catalog' anyway
-app.use('/users', usersRouter); // not used in this project
-app.use('/catalog', catalogRouter); // add catalog routes to middleware chain
+// in this project, route '/' will redirect to '/catalog' anyway
+app.use('/', indexRouter);
+// not used in this project
+app.use('/users', usersRouter);
+// add catalog routes to middleware chain
+app.use('/catalog', catalogRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
